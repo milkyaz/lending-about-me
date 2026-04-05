4f18dd3368b15021e30d9bd8c251bf808566e099
Daniil Zaburunov <zaburunov.2000@mail.ru>
2026-04-05 20:42:26 +0400

    main: add shadcn

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <h2>AppHeader</h2>
+    <div>
+    <Button>Click me</Button>
+  </div>
 </template>
