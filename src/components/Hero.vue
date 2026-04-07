af07b212e302677d49f82d73aaf4419c5ba01e52
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-04-07 22:56:34 +0400

    feature/hero-section: add gradient for hero-badge

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,15 +3,16 @@ import { Badge } from '@/components/ui/badge'
 </script>
 
 <template>
-  <Badge class="hero-badge" variant="default"
-    >Frontend Developer / Vue, TypeScript, Vite</Badge
+  <Badge class="hero-badge" variant="outline"
+    >Frontend Engineer · Vue · TypeScript</Badge
   >
 </template>
 
 <style scoped>
 @reference "@/main.css";
 .hero-badge {
-  @apply w-full flex flex-row justify-start p-2 border border-[#262626] bg-[#121212];
-  @apply [font-family:var(--font-jet)] text-[#a1a1aa] font-medium text-xs;
+  @apply w-full justify-start rounded-full border border-[#2f3b4f] px-3 py-2.25;
+  @apply bg-linear-to-b from-[#0F131A] to-[#151C25];
+  @apply [font-family:var(--font-jet)] text-xs font-medium text-[#b3c0d4];
 }
 </style>
