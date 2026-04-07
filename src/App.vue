94db228818b409c4b3f4c25eac73507be9b47438
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-04-07 22:13:24 +0400

    feature/hero-section: init hero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
+import Hero from '@/components/Hero.vue'
 </script>
 
 <template>
   <div class="bg-[#0a0a0a] px-5.5 py-8">
     <header class="flex flex-row gap-13"><AppHeader /></header>
+    <main><Hero /></main>
   </div>
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Badge } from '@/components/ui/badge'
+</script>
+
+<template>
+  <Badge
+    class="w-full flex flex-row justify-start [font-family:var(--font-jet)] font-medium text-xs"
+    variant="default"
+    >Frontend Developer / Vue, TypeScript, Vite</Badge
+  >
+</template>
+
+<style scoped></style>
