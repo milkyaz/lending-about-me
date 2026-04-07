34a8de849bcdee29b4617960cd802c1622a58ef0
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2026-04-07 22:23:54 +0400

    feature/hero-section: add hero-badge

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Hero from '@/components/Hero.vue'
 </script>
 
 <template>
-  <div class="bg-[#0a0a0a] px-5.5 py-8">
+  <div class="bg-[#0a0a0a] px-5.5 py-8 flex flex-col gap-13">
     <header class="flex flex-row gap-13"><AppHeader /></header>
     <main><Hero /></main>
   </div>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,11 +3,15 @@ import { Badge } from '@/components/ui/badge'
 </script>
 
 <template>
-  <Badge
-    class="w-full flex flex-row justify-start [font-family:var(--font-jet)] font-medium text-xs"
-    variant="default"
+  <Badge class="hero-badge" variant="default"
     >Frontend Developer / Vue, TypeScript, Vite</Badge
   >
 </template>
 
-<style scoped></style>
+<style scoped>
+@reference "@/main.css";
+.hero-badge {
+  @apply w-full flex flex-row justify-start p-2 border border-[#262626] bg-[#121212];
+  @apply [font-family:var(--font-jet)] text-[#a1a1aa] font-medium text-xs;
+}
+</style>
